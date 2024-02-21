f8cda2f1c883574099cf21292020197e5151bcc9
kozlovaAlen <124133196+kozlovaAlen@users.noreply.github.com>
2024-02-22 00:10:39 +0500

    0.1

---
+++ Git/Git/Form1.cs
@@ -0,0 +1,27 @@
+namespace Git
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textToDisplay = textBox1.Text;
+
+            Form newForm = new Form();
+            TextBox textBox = new TextBox();
+            textBox.Text = textToDisplay;
+            textBox.Dock = DockStyle.Fill;
+            newForm.Controls.Add(textBox);
+            newForm.ShowDialog();
+        }
+    }
+}
